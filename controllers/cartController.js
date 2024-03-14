fc511b56f15e3de288b1a67752a680edcdc0ed19
lemueljohnperez0 <lemueljohnperez0@gmail.com>
2024-03-14 10:08:59 +0800

    Updated cart controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -43,7 +43,7 @@ module.exports.addToCart = (req, res) =>{
 		Cart.findOne({ userId })
 		.then(cart => {
 			if (!cart) {
-			// If no cart found, create a new cart and add the product
+				// If no cart found, create a new cart and add the product
 				const newCart = new Cart({
 					userId,
 					cartItems: [{ productId, quantity, subtotal: product.price * quantity }],
@@ -55,23 +55,23 @@ module.exports.addToCart = (req, res) =>{
 					res.status(201).json(savedCart);
 				})
 
-				.catch(error => {
-					res.status(500).json({ error: error.message });
+				.catch(savedCartErr => {
+					res.status(500).json({ error: savedCartErr.message });
 				});
 			}
 
 			else {
-			// If cart exists, check if the product is already in the cart
+				// If cart exists, check if the product is already in the cart
 				const existingCartItem = cart.cartItems.find(item => item.productId.toString() === productId);
 
 				if (existingCartItem) {
-				// If product exists in cart, update the quantity and subtotal
+					// If product exists in cart, update the quantity and subtotal
 					existingCartItem.quantity += quantity;
 					existingCartItem.subtotal = existingCartItem.quantity * product.price;
 				}
 
 				else {
-				// If product does not exist in cart, add it
+					// If product does not exist in cart, add it
 					cart.cartItems.push({ productId, quantity, subtotal: product.price * quantity });
 				}
 
@@ -80,21 +80,75 @@ module.exports.addToCart = (req, res) =>{
 
 				cart.save()
 				.then(updatedCart => {
-					res.json(updatedCart);
+					res.status(200).send({ message: "Cart updated successfully", updatedCart });
 				})
 
-				.catch(error => {
-					res.status(500).json({ error: error.message });
+				.catch(updatedCartErr => {
+					res.status(500).json({ error: updatedCartErr.message });
 				});
 			}
 		})
 
-		.catch(error => {
-			res.status(500).json({ error: error.message });
+		.catch(cartFindErr => {
+			res.status(500).json({ error: cartFindErr.message });
 		});
 	})
 
-	.catch(error => {
-		res.status(500).json({ error: error.message });
+	.catch(productFindErr => {
+		res.status(500).json({ error: productFindErr.message });
 	});
+}
+
+
+// Update Product Quantity
+module.exports.updateCartQuantity = (req, res) => {
+    const userId = req.user.id;
+    const productId = req.body.productId;
+    const quantity = req.body.quantity;
+
+    Product.findById(productId)
+    .then(product => {
+    	if (!product) {
+    		return res.status(404).json({ error: 'Product not found' });
+    	}
+
+    	Cart.findOne({ userId })
+    	.then(cart => {
+    	    if (!cart){
+    	        return res.status(404).json({ message: "No cart found for the user" });
+    	    }
+
+    	    else {
+    	    	const existingCartItem = cart.cartItems.find(item => item.productId.toString() === productId);
+
+    	        if (existingCartItem) {
+    	        	// If product exists in cart, update the quantity and subtotal
+    	            existingCartItem.quantity += quantity;
+    	            existingCartItem.subtotal = existingCartItem.quantity * product.price;
+    	        }
+
+    	        else {
+    	        	// If product does not exist in cart, add it
+    	            cart.cartItems.push({ productId, quantity, subtotal: product.price * quantity });
+    	        }
+
+    	        // Recalculate total price
+    	        cart.totalPrice = cart.cartItems.reduce((total, item) => total + item.subtotal, 0);
+
+    	        cart.save()
+    	        .then(updatedCart => {
+    	        	res.status(200).send({ message: "Cart updated successfully", updatedCart });
+    	        })
+    	        .catch(saveErr => {
+    	        	res.status(500).json({ error: saveErr.message });
+    	        });
+    	    }
+    	})
+    	.catch(cartFindErr => {
+			res.status(500).json({ error: cartFindErr.message });
+		})
+    })
+    .catch(productFindErr => {
+    	res.status(500).json({ error: productFindErr.message });
+    });
 }
